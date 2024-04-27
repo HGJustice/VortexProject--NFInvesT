10db7b72bca237ee79e625453f3512c5a5543882
HGJustice <125309498+HGJustice@users.noreply.github.com>
2024-04-27 21:55:44 +0100

    created sungraph

--- nfinvest/src/business-management.ts
+++ nfinvest/src/business-management.ts
@@ -1,22 +1,22 @@
-import { BusinessCreated as BusinessCreatedEvent } from "../generated/BusinessManagement/BusinessManagement"
-import { BusinessCreated } from "../generated/schema"
+import { BusinessCreated as BusinessCreatedEvent } from "../generated/BusinessManagement/BusinessManagement";
+import { BusinessCreated } from "../generated/schema";
 
 export function handleBusinessCreated(event: BusinessCreatedEvent): void {
   let entity = new BusinessCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.ID = event.params.ID
-  entity.registrationDocuments = event.params.registrationDocuments
-  entity.taxIDNumber = event.params.taxIDNumber
-  entity.proofOfAddress = event.params.proofOfAddress
-  entity.bankAccountNumber = event.params.bankAccountNumber
-  entity.financialDocuments = event.params.financialDocuments
-  entity.anualReports = event.params.anualReports
-  entity.businessWebsite = event.params.businessWebsite
+    event.transaction.hash.toHex().concat("-").concat(event.logIndex.toString())
+  );
+  entity.id = event.params.ID.toString();
+  entity.registrationDocuments = event.params.registrationDocuments;
+  entity.taxIDNumber = event.params.taxIDNumber;
+  entity.proofOfAddress = event.params.proofOfAddress;
+  entity.bankAccountNumber = event.params.bankAccountNumber;
+  entity.financialDocuments = event.params.financialDocuments;
+  entity.anualReports = event.params.anualReports;
+  entity.businessWebsite = event.params.businessWebsite;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
