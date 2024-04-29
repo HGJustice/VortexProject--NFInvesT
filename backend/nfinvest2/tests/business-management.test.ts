c24bf8c916a80ba94cf5d13e949cfaeadb6904b3
HGJustice <125309498+HGJustice@users.noreply.github.com>
2024-04-29 21:16:34 +0100

    Initialize subgraph

--- backend/nfinvest2/tests/business-management.test.ts
+++ backend/nfinvest2/tests/business-management.test.ts
@@ -4,27 +4,27 @@ import {
   test,
   clearStore,
   beforeAll,
-  afterAll,
-} from "matchstick-as";
-import { BigInt } from "@graphprotocol/graph-ts";
-import { BusinessCreated } from "../generated/schema";
-import { BusinessCreated as BusinessCreatedEvent } from "../generated/BusinessManagement/BusinessManagement";
-import { handleBusinessCreated } from "../src/business-management";
-import { createBusinessCreatedEvent } from "./business-management-utils";
+  afterAll
+} from "matchstick-as/assembly/index"
+import { BigInt } from "@graphprotocol/graph-ts"
+import { BusinessCreated } from "../generated/schema"
+import { BusinessCreated as BusinessCreatedEvent } from "../generated/BusinessManagement/BusinessManagement"
+import { handleBusinessCreated } from "../src/business-management"
+import { createBusinessCreatedEvent } from "./business-management-utils"
 
 // Tests structure (matchstick-as >=0.5.0)
 // https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
 
 describe("Describe entity assertions", () => {
   beforeAll(() => {
-    let ID = BigInt.fromI32(234);
-    let registrationDocuments = "Example string value";
-    let taxIDNumber = "Example string value";
-    let proofOfAddress = "Example string value";
-    let bankAccountNumber = BigInt.fromI32(234);
-    let financialDocuments = "Example string value";
-    let anualReports = "Example string value";
-    let businessWebsite = "Example string value";
+    let ID = BigInt.fromI32(234)
+    let registrationDocuments = "Example string value"
+    let taxIDNumber = "Example string value"
+    let proofOfAddress = "Example string value"
+    let bankAccountNumber = BigInt.fromI32(234)
+    let financialDocuments = "Example string value"
+    let anualReports = "Example string value"
+    let businessWebsite = "Example string value"
     let newBusinessCreatedEvent = createBusinessCreatedEvent(
       ID,
       registrationDocuments,
@@ -34,19 +34,19 @@ describe("Describe entity assertions", () => {
       financialDocuments,
       anualReports,
       businessWebsite
-    );
-    handleBusinessCreated(newBusinessCreatedEvent);
-  });
+    )
+    handleBusinessCreated(newBusinessCreatedEvent)
+  })
 
   afterAll(() => {
-    clearStore();
-  });
+    clearStore()
+  })
 
   // For more test scenarios, see:
   // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
 
   test("BusinessCreated created and stored", () => {
-    assert.entityCount("BusinessCreated", 1);
+    assert.entityCount("BusinessCreated", 1)
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
     assert.fieldEquals(
@@ -54,51 +54,51 @@ describe("Describe entity assertions", () => {
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "ID",
       "234"
-    );
+    )
     assert.fieldEquals(
       "BusinessCreated",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "registrationDocuments",
       "Example string value"
-    );
+    )
     assert.fieldEquals(
       "BusinessCreated",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "taxIDNumber",
       "Example string value"
-    );
+    )
     assert.fieldEquals(
       "BusinessCreated",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "proofOfAddress",
       "Example string value"
-    );
+    )
     assert.fieldEquals(
       "BusinessCreated",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "bankAccountNumber",
       "234"
-    );
+    )
     assert.fieldEquals(
       "BusinessCreated",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "financialDocuments",
       "Example string value"
-    );
+    )
     assert.fieldEquals(
       "BusinessCreated",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "anualReports",
       "Example string value"
-    );
+    )
     assert.fieldEquals(
       "BusinessCreated",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "businessWebsite",
       "Example string value"
-    );
+    )
 
     // More assert options:
     // https://thegraph.com/docs/en/developer/matchstick/#asserts
-  });
-});
+  })
+})
