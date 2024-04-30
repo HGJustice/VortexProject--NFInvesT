da73a456dc2f5a98d5f01d100c7fd22437d97392
HGJustice <125309498+HGJustice@users.noreply.github.com>
2024-04-30 02:43:25 +0100

    Initialize subgraph

---
+++ nfinvest4/src/business-management.ts
@@ -0,0 +1,23 @@
+import { BusinessCreated as BusinessCreatedEvent } from "../generated/BusinessManagement/BusinessManagement"
+import { BusinessCreated } from "../generated/schema"
+
+export function handleBusinessCreated(event: BusinessCreatedEvent): void {
+  let entity = new BusinessCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.ID = event.params.ID
+  entity.registrationDocuments = event.params.registrationDocuments
+  entity.taxIDNumber = event.params.taxIDNumber
+  entity.proofOfAddress = event.params.proofOfAddress
+  entity.bankAccountNumber = event.params.bankAccountNumber
+  entity.financialDocuments = event.params.financialDocuments
+  entity.anualReports = event.params.anualReports
+  entity.businessWebsite = event.params.businessWebsite
+  entity.businessLogo = event.params.businessLogo
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
